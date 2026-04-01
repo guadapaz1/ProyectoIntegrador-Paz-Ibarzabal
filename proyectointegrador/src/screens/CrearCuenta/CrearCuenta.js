ab7132d1ef8547bbb9bb52be8bc06fe36c559d7b
guadapaz1 <pazga@udesa.edu.ar>
2026-04-01 11:32:34 -0300

    hice register y login

--- proyectointegrador/src/screens/CrearCuenta/CrearCuenta.js
+++ proyectointegrador/src/screens/CrearCuenta/CrearCuenta.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react'
 
 class CrearCuenta extends Component {
+  constructor(props){
+        super(props);
+        this.state = {
+            email:"",
+            password: "",
+            error:"",
+            
+        }
+    }
+    
+    evitarSubmit(event){
+      event.preventDefault()
+
+    let usuariosGuardados = localStorage.getItem("usuarios");
+    let usuarios = usuariosGuardados ? JSON.parse(usuariosGuardados) : []
+
+    let emailEnUso = false;
+
+    for (let i = 0; i < this.state.usuarios.length; i++) {
+      if (this.state.usuarios[i].email === this.state.email) {
+        emailEnUso = true;
+      }
+    }
+    emailEnUso ? this.setState({error: "El mail ya esta en uso"}):
+    this.state.password.length < 6 ? this.setState({error: "La contraseña debe tener al menos 6 caracteres"}) :
+    (this.state.usuarios.push({
+      email: this.state.email,
+      password: this.state.password
+    }))
+      let usuariosEnString = JSON.stringify(usuarios);
+      localStorage.setItem("usuarios", usuariosEnString);
+
+    }
+
+    controlarCambios(event){
+      this.setState({[event.target.name]: event.target.value})
+    }
+
+
   render() {
     return (
-      <div>CrearCuenta</div>
+      <>
+      <div className="row justify-content-center">
+            <div className="col-md-6">
+                <form onSubmit={(event) => this.evitarSubmit(event)}>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input name="email" onChange= {(event) => this.controlarCambios(event)} value={this.state.email} type="email" className="form-control" id="email" placeholder="Ingresá tu email"/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Contraseña</label>
+                        <input name="password" onChange= {(event) => this.controlarCambios(event)} value={this.state.password} type="password" className="form-control" id="password" placeholder="Ingresá tu contraseña"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Registrarse</button>
+                </form>
+                <p className="mt-3 text-center">¿Ya tenés cuenta? <a href="./Login/Login.js">Iniciar sesión</a></p>
+            </div>
+        </div>
+      
+      </>
     )
   }
 }
--- proyectointegrador/src/screens/Login/Login.js
+++ proyectointegrador/src/screens/Login/Login.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import Registro from  "../../components/Registro/Registro"
 
 class Login extends Component {
 
@@ -10,9 +9,28 @@ class Login extends Component {
         }
     }
     
+    
     render(){
         return(
-            <Registro/>
+           <>
+    <div className="row justify-content-center">
+            <div className="col-md-6">
+                <form >
+                    <div className="form-group">
+                        <label for="email">Email</label>
+                        <input type="email"
+                    className="form-control" id="email" placeholder="Ingresá tu email"/>
+                    </div>
+                    <div className="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" className="form-control" id="password" placeholder="Ingresá tu contraseña"/>
+                    </div>
+                    <button type="submit" className="btn btn-primary btn-block">Iniciar sesión</button>
+                </form>
+                <p className="mt-3 text-center">¿No tenés cuenta? <a href="./CrearCuenta/CrearCuenta.js">Registrarse</a></p>
+            </div>
+        </div>
+        </>
         );
     }
 
