b41727472eb0c7dcac5a6f21a2c556b01bcbf562
guadapaz1 <pazga@udesa.edu.ar>
2026-04-04 19:11:42 -0300

    parte de favoritos

--- proyectointegrador/src/components/Card/Card.js
+++ proyectointegrador/src/components/Card/Card.js
@@ -5,9 +5,69 @@ class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      mostrar: false
+      mostrar: false,
+      esFavorito: false
     };
   }
+componentDidMount() {
+  
+            let storage = localStorage.getItem("favoritos");
+            let storageParseado = JSON.parse(storage);
+
+            let favorito = false;
+
+            if (storageParseado !== null) {
+              for (let i=0; i< storageParseado.length; i++){
+                if(storageParseado[i] === this.props.data.id){
+                  favorito=true
+                }
+              }
+            }
+
+            this.setState({
+                esFavorito: favorito
+            });
+        }
+        
+  
+  agregarFav(id){
+    let storage = localStorage.getItem("favoritos");
+    let storageParseado = JSON.parse(storage);
+
+    if(storageParseado !== null){
+        storageParseado.push(id);
+        let storageString = JSON.stringify(storageParseado);
+        localStorage.setItem("favoritos", storageString);
+    } 
+    else {
+        let primerFav = [id];
+        let storageString = JSON.stringify(primerFav);
+        localStorage.setItem("favoritos", storageString);
+    } 
+    
+    this.setState(
+        {esFavorito: true});
+  }
+
+  sacarFav(id){
+    let storage = localStorage.getItem("favoritos");
+    let storageParseado = JSON.parse(storage);
+
+    let array= []
+
+    for (let i =0; i< storageParseado.length; i++){
+      if (storageParseado[i] !== id){
+        array.push(storageParseado[i])
+      }
+    }
+
+    let storageString = JSON.stringify(array);
+    localStorage.setItem("favoritos", storageString);
+
+    this.setState({
+        esFavorito: false
+    });
+}
 
   mostrarDescripcion() {
     this.setState({
@@ -35,6 +95,16 @@ class Card extends Component {
             )}
 
             <Link to={`/detalle/${this.props.data.title ? "movie" : "tv"}/${this.props.data.id}`}>Ver más</Link>
+
+           {this.state.esFavorito === false ? (
+            <button onClick={() => this.agregarFav(this.props.data.id)}>
+              Agregar a favoritos
+            </button>
+          ) : (
+            <button onClick={() => this.sacarFav(this.props.data.id)}>
+              Eliminar de favoritos
+            </button>
+          )}
 
         </div>
 
